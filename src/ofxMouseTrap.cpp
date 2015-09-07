13031d58972325556c05a1822c27c22b5d8fc2eb
wandergeek <innernetz@gmail.com>
2015-09-07 14:35:40 +1000

    Starting on load function

--- src/ofxMouseTrap.cpp
+++ src/ofxMouseTrap.cpp
@@ -14,12 +14,40 @@ void ofxMouseTrap::recordStop() {
 
 //--------------------------------------------------------------
 bool ofxMouseTrap::load(string filename){
-    //
+    ofFile fileToRead(ofToDataPath(filename));
+    if(!fileToRead.exists()) {
+        ofLog(ofLogLevel::OF_LOG_FATAL_ERROR, filename + " does not exist");
+        return false;
+    }
+    
+    ofxXmlSettings xml;
+    
+    xml.load(filename);
+    numPaths = xml.getNumTags("path");
+    xml.pushTag("path", 0);
+//    int numItems = xml.getNumTags("item");
+    ofLog(ofLogLevel::OF_LOG_NOTICE, "Got " + ofToString(numPaths) + " paths");
+    
+    for(int i=0; i<numPaths; i++) {
+        xml.pushTag("path", i);
+        int numItems = xml.getNumTags("item");
+        vector <MouseEvent> mv;
+        for(int j=0; j<numItems; j++) {
+            MouseEvent m;
+            m.x = xml.getAttribute("item", "x", -1, j);
+            m.y = xml.getAttribute("item", "y", -1, j);
+            m.time = xml.getAttribute("item", "time", -1, j);
+            mv.push_back(m);
+        }
+        paths.push_back(mv);
+    }
+    
+    return true;
 }
 
 //--------------------------------------------------------------
 int ofxMouseTrap::getNumPaths(){
-    //
+    return numPaths;
 }
 
 //--------------------------------------------------------------
@@ -47,8 +75,4 @@ void ofxMouseTrap::update(){
     //
 }
 
-//--------------------------------------------------------------
-void ofxMouseTrap::loadPathVectorFromXML(){
-    //
-}
 
